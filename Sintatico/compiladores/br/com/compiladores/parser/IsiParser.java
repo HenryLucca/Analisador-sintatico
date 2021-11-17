4dbbee4a9d8d862c13733ccb9b0ae5b04e10cfbc
Diogo Ávila <66319119+aviladiogo@users.noreply.github.com>
2021-11-17 16:59:18 -0300

    Update IsiParser.java

--- Sintatico/compiladores/br/com/compiladores/parser/IsiParser.java
+++ Sintatico/compiladores/br/com/compiladores/parser/IsiParser.java
@@ -15,38 +15,47 @@ public class IsiParser {
 
     public void E() {
         token = scanner.nextToken();
-        if (token.getType() == Token.TK_END) {
-          //  System.out.println(token);
-        }
         T();
         El();
 
     }
 
     public void El() {
-        token = scanner.nextToken();
         if (token.getType() != Token.TK_END) {
-            OP();
-            T();
-            E();
-        }else{
-            System.out.println(token);
+            token = scanner.nextToken();
+            if (token != null) {
+                OP();
+                T();
+                El();
+            }
+
+        } else {
+
         }
 
     }
 
     public void T() {
         token = scanner.nextToken();
-        System.out.println(token);
-        if (token.getType() != Token.TK_IDENTIFIER && token.getType() != Token.TK_OPERATOR
-                && token.getType() != Token.TK_FLOAT && token.getType() != Token.TK_NUMBER) {
-            throw new IsiSyntaxException("ID or NUMBER Expected! ");
+        if (token.getType() != Token.TK_END) {
+            if (token.getType() != Token.TK_IDENTIFIER && token.getType() != Token.TK_OPERATOR
+                    && token.getType() != Token.TK_FLOAT && token.getType() != Token.TK_NUMBER
+                    && token.getType() != Token.TK_OP) {
+                throw new IsiSyntaxException("ID or NUMBER Expected!, found " + Token.TK_TEXT[token.getType()] + " ("
+                        + token.getText() + ") at LINE "+token.getLinha()+" at COLUMN "+token.getColuna());
+            }
+        } else {
+
         }
     }
 
     public void OP() {
-        if (token.getType() == Token.TK_OPERATOR) {
-            throw new IsiSyntaxException("Operator Expected");
+        if (token.getType() != Token.TK_ARIT && token.getType() != Token.TK_OPERATOR
+                && token.getType() != Token.TK_OP) {
+            throw new IsiSyntaxException(
+                    "Operator Expected, found " + Token.TK_TEXT[token.getType()] + " (" + token.getText() + ")  at LINE "+token.getLinha()+" at COLUMN "+token.getColuna());
+        } else {
+
         }
     }
 }
