4e22217c869cfa6f6a5bb4c4d674597dcea9dfb6
aviladiogo <aviladiogo30@gmail.com>
2021-11-17 00:03:50 -0300

    atualizações

---
+++ br/com/compiladores/parser/IsiParser.java
@@ -0,0 +1,60 @@
+package br.com.compiladores.parser;
+
+import br.com.compiladores.exceptions.IsiSyntaxException;
+import br.com.compiladores.lexico.IsiScanner;
+import br.com.compiladores.lexico.Token;
+
+public class IsiParser {
+
+    private IsiScanner scanner;
+    private Token token;
+
+    public IsiParser(IsiScanner scanner) {
+        this.scanner = scanner;
+    }
+
+    public void E() {
+        try{
+            token = scanner.nextToken();
+        }catch(Exception ex){
+            System.out.println("erro no next 1");
+        }
+            T();
+            El();
+
+    }
+
+    public void El() {
+        if (token.getType() != Token.TK_END) {
+            token = scanner.nextToken();
+            if(token != null){
+                OP();
+                T();
+                El();
+            }
+            
+        } else {
+            
+        }
+
+    }
+
+    public void T() {
+        token = scanner.nextToken();
+        if (token.getType() != Token.TK_END) {
+            if (token.getType() != Token.TK_IDENTIFIER && token.getType() != Token.TK_OPERATOR && token.getType() != Token.TK_FLOAT && token.getType() != Token.TK_NUMBER&& token.getType() != Token.TK_OP) {
+                throw new IsiSyntaxException("ID or NUMBER Expected!, found "+Token.TK_TEXT[token.getType()]+" ("+token.getText()+")");
+            }
+        } else {
+           
+        }
+    }
+
+    public void OP() {
+        if (token.getType() != Token.TK_ARIT && token.getType() != Token.TK_OPERATOR && token.getType() != Token.TK_OP) {
+           throw new IsiSyntaxException("Operator Expected, found "+Token.TK_TEXT[token.getType()]+" ("+token.getText()+")");
+        }else{
+            
+        }
+    }
+}
